43a45baabdcc4304e2946b42b81ef65d2d7040d3
Laboratório de Ensino de Física <labfis.cup@uft.edu.br>
2025-06-26 12:13:01 -0300

    Roteiro MHS Finalização

---
+++ docs/assets/js/roteiros/fisica-2/massa-mola-mhs/script.js
@@ -0,0 +1,5 @@
+// assets/js/custom.js
+document.addEventListener('DOMContentLoaded', () => {
+  const h2 = document.querySelector(".quarto-alternate-formats h2");
+  if (h2) h2.textContent = "Roteiros em PDF";
+});
